33629c7fbba151b9b6bc4073d823664597a74906
Marco <marco.mangan@gmail.com>
2013-05-10 08:27:07 -0300

    Ajustes na tradução

--- lang/pt/block_simple_clock.php
+++ lang/pt/block_simple_clock.php
@@ -32,13 +32,13 @@ $string['clock_separator'] = ':';
 // Config strings and help
 $string['clock_title_default'] = 'Relógio';
 $string['config_clock_visibility'] = 'Visibilidade dos relógios';
-$string['config_clock_visibility_help'] = '<p>Com esta configuração, controle os relógios que o usuário pode ver:</p>
+$string['config_clock_visibility_help'] = '<p>Defina os relógios que o usuário pode ver:</p>
 <ul>
     <li>horário do servidor,</li>
     <li>horário no seu próprio computador, ou</li>
     <li>ambos.</li>
 </ul>';
-$string['config_header'] = 'Show header';
+$string['config_header'] = 'Mostrar cabeçalho';
 $string['config_header_help'] = '
 <p>With this setting, you can control whether the block header, including the title, will be shown.</p>
 <p style="background:yellow;border:3px dashed black;padding:10px;"><strong>Note</strong><br />
@@ -46,16 +46,16 @@ While "Editing" is turned on, the header of the block will be shown to teachers/
 When "Editing" is turned off, the header will be hidden.
 While this setting set to No, students will not see the header, regardless of whether "Editing" is on or off.</p>
 ';
-$string['config_icons'] = 'Show icons';
+$string['config_icons'] = 'Mostrar ícones';
 $string['config_icons_help'] = '
 <p>With this setting, you can control whether icons are shown next to each clock label.</p>
 <p>The site icon is shown next to the "server" label. The user\'s icon image is shown next to their clock.</p>
 ';
-$string['config_day'] = 'Show day name';
+$string['config_day'] = 'Mostrar o dia';
 $string['config_day_help'] = '
 <p>Showing the day name adds additional information for students who may be in a timezone a day earlier or later.</p>
 ';
-$string['config_seconds'] = 'Show seconds';
+$string['config_seconds'] = 'Mostrar segundos';
 $string['config_seconds_help'] = '
 <p>Showing seconds will show users the time including seconds on all visible clocks.</p>
 <p style="background:yellow;border:3px dashed black;padding:10px;"><strong>Warning</strong><br />
@@ -64,10 +64,10 @@ When this page content arrives at the user\'s browser, the time difference will 
 (usually a few seconds). This is often acceptable as any interaction with Moodle will
 involve a delay, but the delay will be more evident with seconds shown.</p>
 ';
-$string['config_show_both_clocks'] = 'Show clocks for both server and user';
-$string['config_show_server_clock'] = 'Show clock for server only';
-$string['config_show_user_clock'] = 'Show clock for user only';
-$string['config_title'] = 'Alternate title';
+$string['config_show_both_clocks'] = 'Mostrar ambos relógios (servidor e local)';
+$string['config_show_server_clock'] = 'Mostrar apenas o relógio do servidor';
+$string['config_show_user_clock'] = 'Mostrar apenas o relógio local';
+$string['config_title'] = 'Novo título';
 $string['config_title_help'] = '
 <p>This setting allows the block title to be changed.</p>
 <p>If no alternate is provided, the default title is used.</p>
@@ -80,6 +80,6 @@ $string['javascript_disabled'] = 'Habilite o uso de JavaScript no seu navegador 
 $string['loading'] = 'Aguarde...';
 $string['pluginname'] = 'Relógio';
 $string['server'] = 'Servidor';
-$string['simple_clock:myaddinstance'] = 'Add a new Simple Clock block to the My Moodle page';
-$string['simple_clock:addinstance'] = 'Add a new Simple Clock block';
+$string['simple_clock:myaddinstance'] = 'Insira um novo bloco Simple Clock block na página My Moodle';
+$string['simple_clock:addinstance'] = 'Insira um novo bloco Simple Clock';
 $string['you'] = 'Local';
